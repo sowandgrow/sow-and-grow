ed284a86d179519ec59e7bf131b3d96207805af9
kaustubhrx <kaustubhrx@gmail.com>
2023-07-28 12:38:50 +0530

    Add Feat. Splash Screen

---
+++ app/src/main/java/com/example/sowandgrow/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.sowandgrow;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,GetStartedActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
