1b9288226a8386e092921315def43c4f8e5fc90a
kaustubhrx <kaustubhrx@gmail.com>
2023-07-29 15:37:05 +0530

    Add Onboarding Screen

--- app/src/main/java/com/example/sowandgrow/MainActivity2.kt
+++ app/src/main/java/com/example/sowandgrow/MainActivity2.kt
@@ -1,31 +1,30 @@
 package com.example.sowandgrow
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.sowandgrow.databinding.ActivityMainBinding
+import com.example.sowandgrow.databinding.ActivityMain2Binding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
-    private var binding:ActivityMainBinding? = null
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
     private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding?.root)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
         auth = Firebase.auth
 
-
-        binding?.btnSignOut?.setOnClickListener{
-            if (auth.currentUser!= null)
-            {
+        binding.btnSignOut.setOnClickListener {
+            if (auth.currentUser != null) {
                 auth.signOut()
-                startActivity(Intent(this,GetStartedActivity::class.java))
+                startActivity(Intent(this, GetStartedActivity::class.java))
                 finish()
             }
         }
-
     }
 }
