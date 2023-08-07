366dac4b97759f6a859c60f1316b86f4901e1664
Sejal Rane <ranesejal94@gmail.com>
2023-08-07 23:10:09 +0530

    Modified search fragment and Added Plant Recycler view

--- app/src/main/java/com/example/sowandgrow/Search.kt
+++ app/src/main/java/com/example/sowandgrow/Search.kt
@@ -1,23 +1,16 @@
 package com.example.sowandgrow
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.textfield.TextInputEditText
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Home.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Search : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -29,24 +22,28 @@ class Search : Fragment() {
         }
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
+
+        // Set up the click listener for TextInputEditText
+        val nameEditText: TextInputEditText = view.findViewById(R.id.searchBox)
+        nameEditText.setOnClickListener {
+            val intent = Intent(activity, PlantRecyclerView::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Home.
-         */
-        // TODO: Rename and change types and number of parameters
+        private const val ARG_PARAM1 = "param1"
+        private const val ARG_PARAM2 = "param2"
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             Search().apply {
@@ -57,5 +54,6 @@ class Search : Fragment() {
             }
     }
 }
+
 
 
