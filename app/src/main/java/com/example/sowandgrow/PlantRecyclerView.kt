29ba85640912dce20388b38c898e3b57e99574f6
Sejal Rane <ranesejal94@gmail.com>
2023-08-16 21:45:18 +0530

    Revert "recyclerview xml added"
    This reverts commit 515ef8bcf9cadfcad272271bbad46524bcef0d65.

---
+++ app/src/main/java/com/example/sowandgrow/PlantRecyclerView.kt
@@ -0,0 +1,11 @@
+package com.example.sowandgrow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlantRecyclerView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plant_recycler_view)
+    }
+}
