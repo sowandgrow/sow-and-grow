d681605d4e13e9f40c1479ecd4b457d6596db223
Megha Patel <megha002509@gmail.com>
2023-08-26 21:07:22 +0530

    background change

--- app/src/main/java/com/example/sowandgrow/plantdetails/AglaonemaPlant.kt
+++ app/src/main/java/com/example/sowandgrow/plantdetails/AglaonemaPlant.kt
@@ -1,12 +1,17 @@
 package com.example.sowandgrow.plantdetails
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sowandgrow.MyGarden
 import com.example.sowandgrow.R
+import com.google.android.material.button.MaterialButton
 
 class AglaonemaPlant : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aglaonemaplant)
+
+
     }
 }
